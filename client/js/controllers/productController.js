27c8545c2deb5d6b5c4c95d173328edcf04b6334
mark malone <warhamm3r@marks-MacBook.local>
2017-07-18 09:51:51 -0500

    Added ng-model with placeholders into the views --added a factory for products___(Non-functional);

--- client/js/controllers/productController.js
+++ client/js/controllers/productController.js
@@ -1,4 +1,14 @@
 
-app.controller('productController', function($scope) {
-    console.log("Products Page Loaded")
-})
+// app.controller('productController',"$scope","$routeParams","$location", 
+// function($scope) {
+//     console.log("Products Page Loaded");
+//     $scope.products = products.query();
+// })
+
+    app.controller("productController", ["$scope", "products" ,"$routeParams" 
+    ,"$location",
+    function($scope, products, $routeParams, $location){
+        console.log("Products page and controller loaded!")
+        $scope.products = products.query();
+        console.log(products.query());
+    }])
