e24a6e5659c3d4f56f194506b7bd9d7ed0332204
Clay Terry <leonardclaytonterry@gmail.com>
2017-07-19 10:29:05 -0500

    Fixes purchases.ctrl

--- server/controllers/purchases.ctrl.js
+++ server/controllers/purchases.ctrl.js
@@ -1,18 +1,23 @@
 var express = require('express'); 
 var procedures = require('../procedures/purchases.proc');
+var stripeSvc = require('../services/stripe.svc');
 
 var router = express.Router(); 
 
 //  --> /api/purchases/
 route.post('/', function(req, res) {
-     
-    procedures.write(req.body.purchaseId, req.body.price, req.body.stripeTransactionId)
-        .then(function(data) {
-            console.log('Purchase History Saved'); 
-            res.send(data);
+     stripeSvc.charge(req.body.token, req.body.amount)
+         .then(function (success) {
+             procedures.write(req.body.purchaseId, req.body.amount, req.body.stripeTransactionId)
+                 .then(function (data) {
+                     console.log('Purchase History Saved');
+                     res.send(data);
+                 }, function (err) {
+                     console.log('Something went wrong trying to purchase: ' + err.message);
+                 })
         }, function(err) {
-            console.log('Something went wrong trying to purchase: ' + err.message);
-        })
+            console.log('Something went wrong trying to charge the card: ' + err.message);
+        })   
 })
 
 module.exports = router;
