2685ef9201ac005a2031ab196771e5b3b7948d5b
Mark Andrew Scofield <mscofield16@icloud.com>
2017-07-19 12:23:48 -0500

    changes to contactController.js/contact.html

--- client/js/controllers/contactController.js
+++ client/js/controllers/contactController.js
@@ -3,3 +3,24 @@ app.controller('contactController', function($scope) {
     console.log("Contact Page Loaded")
 })
 
+.controller("contactController", ['$scope','EmailFactory', function($scope, EmailFactory){
+    $scope.contact = "CONTACT ME";
+
+    $scope.getMessage = function() {
+        var newEmail = {
+            to: 'lclaytont@gmail.com',
+            from: $scope.email,
+            subject: $scope.subject,
+            content: $scope.content
+        }
+        console.log(newEmail);
+
+        var materEmail = new EmailFactory(newEmail);
+        masterEmail.$save(function (){
+            console.log("sent email")
+        }, function(){
+            console.log("Error sending email")
+        })
+    }
+}])
+
