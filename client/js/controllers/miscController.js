33f31c7ccd10b3ca2a434c87f5f8cc91d0b0c71a
Clay Terry <leonardclaytonterry@gmail.com>
2017-07-20 13:24:42 -0500

    further down the road on the cart

--- client/js/controllers/miscController.js
+++ client/js/controllers/miscController.js
@@ -4,41 +4,47 @@ app.controller('miscController',
         
         $scope.misc = miscService.query();
 
-        console.log($scope.misc);
+        // console.log($scope.misc);
 
         $scope.addToCart = function (m) {
 
-            console.log(m);
+            // console.log(m);
             
             var cart = JSON.parse(localStorage.cart);
+            var index = indexOf(cart, m);
+            console.log(index);
 
             if (cart.length === 0){
+                m.quantity = 1;
                 cart.push(m); 
                 localStorage.setItem('cart', JSON.stringify(cart));
             } else {
-                cart.push(m);
-                localStorage.removeItem('cart');
-                localStorage.setItem('cart', JSON.stringify(cart));
+                if (index === -1) {
+                    // cart[index].quantity = cart[index].quantity + 1;
+                    m.quantity = 1;
+                    localStorage.removeItem('cart');
+                    cart.push(m);
+                    localStorage.setItem('cart', JSON.stringify(cart));
+                } else {
+                    cart[index].quantity += 1;
+                    cart.push(m);
+                    localStorage.setItem("cart", cart);
+                }
+                // cart.push(m);
+                // localStorage.removeItem('cart');
+                // localStorage.setItem('cart', JSON.stringify(cart));
             }
             
-            // var index = indexOf(localStorage.cart, m);
-
-            // if (index != -1) {
-            //     $scope.cart[index].quantity = $scope.cart[index].quantity + 1;
-            // } else {
-            //     m.quantity = 1;
-            //     $scope.cart.push(m);
-            //     localStorage.setItem("cart", $scope.cart);
-            // }
-
-        //     function indexOf(array, object) {
-        //         for (var i = 0; i < array.length; i++) {
-        //         if (array[i].id === object.id) {
-        //             return i;
-        //         }
-        //         return -1;
-        //     }
-        // }
+            function indexOf(array, object) {
+                for (var i = 0; i < array.length; i++) {
+                if (array[i].product_id === object.product_id) {
+                    console.log(array[1].product_id)
+                    return i;
+                    
+                }
+                return -1;
+            }
+        }
 
         console.log(localStorage);
     }
